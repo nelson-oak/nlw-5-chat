a434024094afd15bc0462f81206fcf40109209c1
Nelson Carvalho <nelson.carvalho.13@outlook.com>
2021-04-23 20:05:40 -0300

    Comunicaçao atendente - cliente

--- src/http.ts
+++ src/http.ts
@@ -22,7 +22,10 @@ app.set('view engine', 'html');
 // Renderizar tela
 app.get('/pages/client', (request, response) => {
   return response.render('html/client.html');
-})
+});
+app.get('/pages/admin', (request, response) => {
+  return response.render('html/admin.html');
+});
 
 app.use(express.json())
 
--- src/server.ts
+++ src/server.ts
@@ -1,5 +1,6 @@
 import { http } from './http';
 import './websocket/client';
+import './websocket/admin';
 
 // Trocar app.listen() por http.listen para iniciar o servidor
 http.listen(3333, () => console.log('🚀️ Server is running on port 3333 🚀️'));
